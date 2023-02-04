2753e77f21f6f577a07903798b80d60da44533af
tutkawari <riki515001@gmail.com>
2023-02-05 01:07:38 +0900

    for pull request

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,27 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
+        #A<B
         def test_sample1 (self):
                 self.assertEqual (21, calc(3,7))
-
+        #A>B
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
+                self.assertEqual (48, calc(8,6))
+        #A=0
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
+                self.assertEqual (-1, calc(0,150))
+        #B=0
         def test_sample4 (self):
+                self.assertEqual (-1, calc(23,0))
+        #Bismax
+        def test_sample5 (self):
+                self.assertEqual (999, calc(1,999))
+        #Bis1000
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(1,1000))
+        #B:string
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(3,'b'))
+        #A is not int
+        def test_sample8 (self):
                 self.assertEqual (-1, calc(0.1,999))
-
